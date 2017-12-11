04d293586fbe372bb605121aadb6ead927fd85ef
Ben Lambert <ben@benjaminlambert.com>
2017-12-10 23:12:51 -0500

    Fix ILabelCompareTest.

--- src/test/java/com/javafst/operations/ILabelCompareTest.java
+++ src/test/java/com/javafst/operations/ILabelCompareTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Comparator;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import com.javafst.Arc;
 import com.javafst.State;
@@ -13,14 +13,12 @@ public class ILabelCompareTest {
   
   @Test
   public void testCompare () {
-    //Comparator<Arc> comparator = new ILabelCompare();
-    ILabelCompare comparator = new ILabelCompare();
+    Comparator<Arc> comparator = new ILabelCompare();
     Arc arc1 = new Arc(0, 0, 0.0f, new State(0.0f));
     Arc arc2 = new Arc(1, 1, 0.0f, new State(0.0f));
     
-    assertEquals(1, comparator.compare(arc1, arc2));
-    assertEquals(-1, comparator.compare(arc2, arc1));
-
+    assertEquals(-1, comparator.compare(arc1, arc2));
+    assertEquals(1, comparator.compare(arc2, arc1));
     assertEquals(1, comparator.compare(null, arc2));
     assertEquals(-1, comparator.compare(arc1, null));    
   }
