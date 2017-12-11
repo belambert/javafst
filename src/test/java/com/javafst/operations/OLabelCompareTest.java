e5362ae650e96c8caf498abb581de7cd9566d91c
Ben Lambert <ben@benjaminlambert.com>
2017-12-10 23:31:09 -0500

    Adding OLabelCompareTest.

--- src/test/java/com/javafst/operations/OLabelCompareTest.java
+++ src/test/java/com/javafst/operations/OLabelCompareTest.java
@@ -2,17 +2,18 @@ package com.javafst.operations;
 
 import static org.junit.Assert.assertEquals;
 
-import com.javafst.Arc;
-import com.javafst.State;
 import java.util.Comparator;
+
 import org.testng.annotations.Test;
 
+import com.javafst.Arc;
+import com.javafst.State;
 
-public class ILabelCompareTest {
-  
+public class OLabelCompareTest {
+
   @Test
   public void testCompare () {
-    Comparator<Arc> comparator = new ILabelCompare();
+    Comparator<Arc> comparator = new OLabelCompare();
     Arc arc1 = new Arc(0, 0, 0.0f, new State(0.0f));
     Arc arc2 = new Arc(1, 1, 0.0f, new State(0.0f));
     
@@ -22,5 +23,5 @@ public class ILabelCompareTest {
     assertEquals(1, comparator.compare(null, arc2));
     assertEquals(-1, comparator.compare(arc1, null));
   }
-
+  
 }
